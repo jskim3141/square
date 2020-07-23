fc77f8cbe26539d850fcbbd9a6ecbe68ed7ddbd7
준서 <68636098+jskim3141@users.noreply.github.com>
2020-07-23 16:10:40 +0900

    Add files via upload

---
+++ square_result.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      if($_GET['id']==1){
+        echo 'true';
+      }else{
+        echo 'false';
+      };
+     ?>
+  </body>
+</html>
